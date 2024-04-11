1933a8a80e4a75f59337399761e415004d8a26d1
overguy723 <156625964+overguy723@users.noreply.github.com>
2024-04-11 17:36:51 +0900

    240411_업데이트
    <Back>
    경매(Auction) Domain, DTO, Service/Impl, Controller 작성 완료
    각종 설정에 필요한 CustomServletConfig, RootConfig, CustomControllerAdvice, LocalDateTimeFormatter 작성
    
    <Front>
    yarn 설치
    tailwindcss 설치, tailwind.config 파일 작성, index.css에 tailwind 관련 설정 추가

--- recommerceAPI/src/main/java/com/recommerceAPI/config/CustomServletConfig.java
+++ recommerceAPI/src/main/java/com/recommerceAPI/config/CustomServletConfig.java
@@ -2,6 +2,7 @@ package com.recommerceAPI.config;
 
 import com.recommerceAPI.controller.formatter.LocalDateTimeFormatter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class CustomServletConfig implements WebMvcConfigurer {
@@ -10,5 +11,14 @@ public class CustomServletConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
 
         registry.addFormatter(new LocalDateTimeFormatter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(300)
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
     }
 }
--- recommerceAPI/src/main/java/com/recommerceAPI/controller/AuctionController.java
+++ recommerceAPI/src/main/java/com/recommerceAPI/controller/AuctionController.java
@@ -4,12 +4,14 @@ import com.recommerceAPI.dto.AuctionDTO;
 import com.recommerceAPI.dto.PageRequestDTO;
 import com.recommerceAPI.dto.PageResponseDTO;
 import com.recommerceAPI.service.AuctionService;
+import com.recommerceAPI.util.CustomFileUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuctionController {
 
     private final AuctionService service;
+    private final CustomFileUtil customFileUtil;
 
     @GetMapping("/{apno}")
     public AuctionDTO get(@PathVariable(name = "apno") Long apno) {
@@ -31,5 +34,44 @@ public class AuctionController {
         log.info(pageRequestDTO);
 
         return service.list(pageRequestDTO);
+    }
+
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName){
+
+        return customFileUtil.getFile(fileName);
+    }
+
+//    @PreAuthorize("hasAnyRole('ADMIN')")
+    @PostMapping("/")
+    public Map<String, Long> register(AuctionDTO auctionDTO) {
+        log.info("AuctionDTO: " + auctionDTO);
+
+        Long apno = service.register(auctionDTO);
+
+        return Map.of("APNO", apno);
+    }
+
+//    @PreAuthorize("hasAnyRole('ADMIN')")
+    @PutMapping("/{apno}")
+    public Map<String, String> modify(@PathVariable(name="apno") Long apno,
+                                      AuctionDTO auctionDTO) {
+        auctionDTO.setApno(apno);
+
+        log.info("Modify: " + auctionDTO);
+
+        service.modify(auctionDTO);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+//    @PreAuthorize("hasAnyRole('ADMIN')")
+    @DeleteMapping("/{apno}")
+    public Map<String, String> remove(@PathVariable(name="apno") Long apno) {
+        log.info("Remove: " + apno);
+
+        service.remove(apno);
+
+        return Map.of("RESULT", "SUCCESS");
     }
 }
