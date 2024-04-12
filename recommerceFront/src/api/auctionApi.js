411b9342131e5c8aa258e2d9ea274da78d93ecaf
overguy723 <156625964+overguy723@users.noreply.github.com>
2024-04-12 17:44:01 +0900

    240415_리베이스
    test 브랜치 수정 내용 적용

--- recommerceFront/src/api/auctionApi.js
+++ recommerceFront/src/api/auctionApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { API_SERVER_HOST } from "./userApi";
+export const API_SERVER_HOST = "http://localhost:8080"; // userApi에 host 작성 시 삭제 예정
+// import { API_SERVER_HOST } from "./userApi";
 
 const host = `${API_SERVER_HOST}/auction`;
 
@@ -11,7 +12,7 @@ export const getList = async (pageParam) => {
     const res = await axios.get(`${host}/list`, { params: { page, size } });
     return res.data;
   } catch (error) {
-    console.error("Error fetching product list:", error);
+    console.error("Error fetching auction list:", error);
     throw error; // 오류를 호출자에게 전파
   }
 };
@@ -26,6 +27,7 @@ export const postOne = async (product) => {
   const header = { headers: { "Content-Type": "multipart/form-data" } };
 
   const res = await axios.post(`${host}/`, product, header);
+  // const res = await jwtAxios.post(`${host}/`, auction, header); // 권한 작업 후 jwtAxios로 수정 예정
 
   return res.data;
 };
@@ -34,12 +36,14 @@ export const putOne = async (pno, product) => {
   const header = { headers: { "Content-Type": "multipart/form-data" } };
 
   const res = await axios.put(`${host}/${pno}`, product, header);
+  // const res = await jwtAxios.put(`${host}/${apno}`, auction, header); // 권한 작업 후 jwtAxios로 수정 예정
 
   return res.data;
 };
 
 export const deleteOne = async (product) => {
   const res = await axios.delete(`${host}/${product.pno}`, {
+    // const res = await jwtAxios.delete(`${host}/${auction.apno}`, { // 권한 작업 후 jwtAxios로 수정 예정
     data: product,
   });
 
