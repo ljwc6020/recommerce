d172ea35b3b6ef1a931976fa4c1459b17801e5b1
ljwc6020 <156625913+ljwc6020@users.noreply.github.com>
2024-04-12 09:41:40 +0900

    1:다 채팅, 알림까지
    지금은 이렇게쓰고 나중에 로그인상태로 받아오도록 수정함

--- recommerceAPI/src/main/java/com/recommerceAPI/config/ChatHandler.java
+++ recommerceAPI/src/main/java/com/recommerceAPI/config/ChatHandler.java
@@ -10,10 +10,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Log4j2
 @Component
@@ -23,17 +20,24 @@ public class ChatHandler extends TextWebSocketHandler {
 
     private final  Set<WebSocketSession> sessions = new HashSet<>();
 
-    private final Map<String, WebSocketSession> sessionToUsername = new HashMap<>();
+    private final Map<String, List<WebSocketSession>> sessionToUsername = new HashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         sessions.add(session);
         log.info("{} connect", session.getId());
-
         // 여기서 세션과 사용자 이름을 매핑하고 저장
         URI uri = session.getUri();
         String room = extractRoomFromUri(uri);
-        sessionToUsername.put(room, session);
+        List<WebSocketSession> sessionList = sessionToUsername.getOrDefault(room, new ArrayList<>());
+        sessionList.add(session);
+        sessionToUsername.put(room, sessionList);
+        String author = "알림";
+        String message = "유저 입장";
+        String time = " ";
+        ChatMessageDTO notificationDTO = new ChatMessageDTO(room, author, message, ChatMessageDTO.MessageType.NOTIFICATION,time );
+
+        sendMessageToRoom(room,notificationDTO,session);
     }
 
     private String extractRoomFromUri(URI uri) {
@@ -71,30 +75,40 @@ public class ChatHandler extends TextWebSocketHandler {
             return;
         }
 
-            sendMessageToRoom(room, chatMessageDTO);
-
+        sendMessageToRoom(room, chatMessageDTO, session); // 수정된 부분
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
+
+        URI uri = session.getUri();
+        String room = extractRoomFromUri(uri);
+
+        String author = "알림";
+        String message = "유저 퇴장";
+        String time = " ";
+        ChatMessageDTO notificationDTO = new ChatMessageDTO(room, author, message, ChatMessageDTO.MessageType.NOTIFICATION,time );
+
+        sendMessageToRoom(room,notificationDTO,session);
+
         sessions.remove(session);
         sessionToUsername.remove(session.getId());
         log.info("{} disconnect", session.getId());
     }
 
     // handleTextMessage 메서드 내부에서 사용자가 입력한 채팅을 해당 방의 모든 세션에게 전달하는 부분입니다.
-    private void sendMessageToRoom(String room, ChatMessageDTO messageDTO) {
+    private void sendMessageToRoom(String room, ChatMessageDTO messageDTO, WebSocketSession senderSession) {
         log.info("=============================" + messageDTO);
         log.info("======================" + sessionToUsername);
         log.info("=============================" + room);
 
-        // 방 번호와 일치하는 세션에 메시지를 보냅니다.
-        for (Map.Entry<String, WebSocketSession> entry : sessionToUsername.entrySet()) {
-            String sessionRoom = entry.getKey();
-            log.info("============================"+sessionRoom);
-            if (room.equals(sessionRoom)) {
-                WebSocketSession session = entry.getValue();
-                sendMessage(session, messageDTO);
+        // 방 번호와 일치하는 모든 세션에 메시지를 보냅니다.
+        List<WebSocketSession> sessionsInRoom = sessionToUsername.get(room);
+        if (sessionsInRoom != null) {
+            for (WebSocketSession session : sessionsInRoom) {
+                if (!session.getId().equals(senderSession.getId())) { // 자기 자신에게 보낸 메시지는 제외
+                    sendMessage(session, messageDTO);
+                }
             }
         }
     }
